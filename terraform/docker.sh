f132e086744427839348f96ed0f24f7b28778295
Oleksii Yesin <oleksiiyesin@MacBook-Air-Oleksii.local>
2021-10-07 00:02:53 +0300

    v1

--- terraform/docker.sh
+++ terraform/docker.sh
@@ -12,4 +12,4 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo docker run -p 8000:8000 -d yesinaleksey/weatherapp:v5
+sudo docker run -p 8000:8000 -d yesinaleksey/weatherapp:v1
